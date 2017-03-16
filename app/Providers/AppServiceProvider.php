2c1ca3a975b93e742e2025c21a41cec6c16b22b1
lintume <lintume@gmail.com>
2017-03-16 23:14:10 +0200

    fix boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Doctrine\DBAL\Schema\Schema;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
