cec2ba09689aea237980b8f3a62b009aba5ab74f
lintume <lintume@gmail.com>
2017-03-17 21:47:01 +0200

    fix blade

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8">
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-    <link href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
-    <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
--- resources/views/stegonography.blade.php
+++ resources/views/stegonography.blade.php
@@ -14,7 +14,7 @@
         }
         var data = google.visualization.arrayToDataTable(dataChart, true);
         var options = {
-            title: 'LSB',
+            title: 'IF',
             hAxis: {title: 'Bits',  titleTextStyle: {color: '#333'}},
             vAxis: {minValue: 0.999999, maxValue: 1}
         };
@@ -23,26 +23,55 @@
         chart.draw(data, options);
     }
 </script>
-<table>
-    <tr>
-        <td>
-            <img src="{{$originalSrc}}">
-        </td>
-        @foreach($crypto as $image)
-        <td>
-            <img src="{{$image}}">
-        </td>
-            @endforeach
-    </tr>
-    <tr>
-        @foreach($crypto as $image => $key)
-            <td>
-                {{$key}}
-            </td>
-        @endforeach
-            <td>
-                original
-            </td>
-    </tr>
-</table>
-<div id="chart_div" style="width: 100%; height: 500px;"></div>
+
+<ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">LSB</a></li>
+    <li><a data-toggle="tab" href="#menu1">Блочное встраивание</a></li>
+    <li><a data-toggle="tab" href="#menu2">Квантование</a></li>
+    <li><a data-toggle="tab" href="#menu3">Крест</a></li>
+    <li><a data-toggle="tab" href="#menu4">Коха-Жао</a></li>
+</ul>
+
+<div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+        <table>
+            <tr>
+                <td>
+                    <img src="{{$originalSrc}}">
+                </td>
+                @foreach($crypto as $image)
+                    <td>
+                        <img src="{{$image}}">
+                    </td>
+                @endforeach
+            </tr>
+            <tr>
+                @foreach($crypto as $image => $key)
+                    <td>
+                        {{$key}}
+                    </td>
+                @endforeach
+                <td>
+                    original
+                </td>
+            </tr>
+        </table>
+        <div id="chart_div" style="width: 100%"></div>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+        <h3>Блочное встраивание</h3>
+        <p>Some content in menu 1.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+        <h3>Квантование</h3>
+        <p>Some content in menu 2.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+        <h3>Крест</h3>
+        <p>Some content in menu 2.</p>
+    </div>
+    <div id="menu4" class="tab-pane fade">
+        <h3>Коха-Жао</h3>
+        <p>Some content in menu 2.</p>
+    </div>
+</div>
